e44fdaa8c854d36f9a8a284e7222e541d14a3b10
jhons1206 <webdeveloper84@gmail.com>
2023-03-09 08:40:39 -0500

    Sección 53: Creando Un Proyecto c/ Node Express Sequelize Pug y Bootstrap - Cap. 397

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // req: lo que enviamos - res: lo que express nos responde
-    res.send('Inicio'); // .send: Método utilizado para mostrar algo en pantalla
+    res.render('Inicio'); // .send: Método utilizado para mostrar algo en pantalla
 });
 router.get('/nosotros', (req, res) => {
-    res.render('nosotros'); // Renderiza una vista
+    // Renderiza una vista
+    res.render('nosotros');
 });
 
 export default router;
-
-// Muestra un objeto en formato .json
-// res.json({
-//     id: 1,
-// });
