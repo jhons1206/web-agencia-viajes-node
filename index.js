d1086ea42c5dc996518677813a157426e20d6672
jhons1206 <webdeveloper84@gmail.com>
2023-04-02 09:10:50 -0500

    PROYECTO: Introduccion a MVC creando un modelo - Cap. 403

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 // Sintaxis common.js
 // const express = require('express');
+
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada') )
+    .catch( error => console.log(error));
+    
 
 // Definir puerto
 const port = process.env.PORT || 4000;
 
 // Habilitar Pug (Template Engine)
 app.set('view engine', 'pug');
+
+// Obtener año actual
+app.use((req, res, next) => {
+    const year = new Date();
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+    next();
+});
+
+// Definir la carpeta publica
+app.use(express.static('public'));
 
 // Agregar Router
 app.use('/', router);
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,28 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // req: lo que enviamos - res: lo que express nos responde
-    res.render('Inicio'); // .send: Método utilizado para mostrar algo en pantalla
+    res.render('Inicio', {
+        pagina: 'Inicio'
+    }); // .send: Método utilizado para mostrar algo en pantalla
 });
+
 router.get('/nosotros', (req, res) => {
     // Renderiza una vista
-    res.render('nosotros');
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
+});
+
+router.get('/viajes', (req, res) => {
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req, res) => {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales',
+    });
 });
 
 export default router;
