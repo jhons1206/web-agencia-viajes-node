c1f89f4c43b933aaa47cc2a3ba15629036ae57bd
jhons1206 <webdeveloper84@gmail.com>
2023-04-08 07:29:11 -0500

    PROYECTO: Cap. 411: Almacenar datos en la BD, Cap. 412: Consultar testimoniales BD, Cap. 413: Mostrar testimoniales en la vista

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,5 +1,6 @@
+import { Testimonial } from '../models/Testimoniales.js';
 
-const guardarTestimonial = (req, res) => {
+const guardarTestimonial = async (req, res) => {
 
     // Validar formulario
     const { nombre, correo, mensaje } = req.body;
@@ -20,11 +21,33 @@ const guardarTestimonial = (req, res) => {
 
     if(errores.length > 0) {
 
+        // Consultar testimoniales existentes
+        const testimoniales = await Testimonial.findAll();
+
         // Mostrar la vista con errores
         res.render('testimoniales', {
             pagina: 'Testimoniales',
-            errores
+            errores,
+            nombre,
+            correo,
+            mensaje,
+            testimoniales
         })
+    } else {
+        // Almacenarlo en la base de datos
+
+        try {
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+
+            res.redirect('/testimoniales');
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
 
